103da8056fa9a4e8bfa86fe93db60440c4ac7549
Ramon Bartl <rb@ridingbytes.com>
2023-05-01 08:23:49 +0200

    Add tests for pentra xlr

---
+++ src/senaite/astm/tests/base.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+import asyncio
+import os
+from glob import glob
+from unittest import IsolatedAsyncioTestCase
+
+from senaite.astm.constants import ACK
+from senaite.astm.constants import CRLF
+from senaite.astm.constants import ENQ
+
+
+class ASTMTestBase(IsolatedAsyncioTestCase):
+    """Base Test Class
+    """
+    HOST = "localhost"
+    PORT = 7980
+
+    @property
+    def data_dir(self):
+        """Returns the data directory
+        """
+        test_path = os.path.dirname(__file__)
+        return os.path.join(test_path, "data")
+
+    @property
+    def instrument_files(self):
+        """Returns all instrument files from the data directory
+        """
+        path = "{}/*.txt".format(self.data_dir)
+        return glob(path)
+
+    def get_instrument_file_path(self, filename):
+        """Returns the instrument file path
+        """
+        return os.path.join(self.data_dir, filename)
+
+    def read_file_lines(self, path, mode="rb"):
+        """Read the lines of the file
+        """
+        with open(path, mode) as f:
+            return f.readlines()
+
+    async def communicate(self, data, **kw):
+        """Simulate a full instrument communication
+        """
+        reader = kw.get("reader", None)
+        writer = kw.get("writer", None)
+        if not all([reader, writer]):
+            reader, writer = await asyncio.open_connection(
+                self.HOST, self.PORT)
+
+        # Start with an ENQ
+        writer.write(ENQ)
+        await writer.drain()
+        response = await reader.read(100)
+        self.assertEqual(response, ACK)
+
+        # Send instrument data
+        for line in data:
+            # Test fixture: Remove trailing \r\n
+            message = line.strip(CRLF)
+            writer.write(message)
+            await writer.drain()
+            response = await reader.read(100)
+            # We expect an ACK as response
+            self.assertEqual(response, ACK)
+
+        # close the connection
+        writer.close()
+        await writer.wait_closed()
