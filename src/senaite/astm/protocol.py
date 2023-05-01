f7256b410f251f80558328a2c2847a651128f811
Ramon Bartl <rb@ridingbytes.com>
2023-05-01 20:37:47 +0200

    Environment no longer required

--- src/senaite/astm/protocol.py
+++ src/senaite/astm/protocol.py
@@ -11,104 +11,60 @@ from senaite.astm.constants import NAK
 from senaite.astm.constants import STX
 from senaite.astm.exceptions import InvalidState
 from senaite.astm.exceptions import NotAccepted
-from senaite.astm.utils import CleanupDict
 from senaite.astm.utils import is_chunked_message
 from senaite.astm.utils import join
 from senaite.astm.utils import make_checksum
 from senaite.astm.utils import write_message
 
 TIMEOUT = 15
+QUEUE = asyncio.Queue()
+DEFAULT_FORMAT = "lis2a"
 
 
 class ASTMProtocol(asyncio.Protocol):
     """ASTM Protocol
 
     Responsible for communication and collecting complete and valid messages.
+
+    NOTE: Every connection must be handled by an own instance of this protocol!
     """
     def __init__(self, **kwargs):
         logger.debug("ASTMProtocol:constructor")
-        self.queue = asyncio.Queue()
-        self.environ = CleanupDict()
         self.loop = asyncio.get_event_loop()
+        self.queue = kwargs.get("queue", QUEUE)
         self.timeout = kwargs.get("timeout", TIMEOUT)
-        self.message_format = kwargs.get("message_format", "lis2a")
+        self.message_format = kwargs.get("message_format", DEFAULT_FORMAT)
+
+        self.transport = None
+        self.client = None
+        self.timer = None
+        self.chunks = []
+        self.messages = []
+        self.in_transfer_state = False
 
     def connection_made(self, transport):
         """Called when a connection is made.
         """
         self.transport = transport
+        # Remember the connected client
+        self.client = self.get_client_key(transport)
         # Invoke on_timeout callback *after* the given time.
         self.timer = self.loop.call_later(self.timeout, self.on_timeout)
-        logger.info("Connection from {!s}".format(self.client))
+        logger.debug("Connection from {!s}".format(self.client))
 
-    @property
-    def client(self):
-        peername = self.transport.get_extra_info("peername")
+    def get_client_key(self, transport):
+        """Return the client key for the given transport
+        """
+        peername = transport.get_extra_info("peername")
         return "{:s}:{:d}".format(*peername)
-
-    @property
-    def env(self):
-        """Returns the environment for the current connected client
-        """
-        if self.client not in self.environ:
-            self.environ[self.client] = {
-                "chunks": [],
-                "messages": [],
-                "in_transfer_state": False,
-            }
-        return self.environ[self.client]
-
-    @env.setter
-    def env(self, value):
-        self.environ[self.client] = value
-
-    def get_message_queue(self):
-        """Queue used for message dispatching
-        """
-        return self.queue
-
-    @property
-    def chunks(self):
-        try:
-            return self.env["chunks"]
-        except KeyError:
-            chunks = []
-            self.env["chunks"] = chunks
-            return chunks
-
-    @chunks.setter
-    def chunks(self, value):
-        self.env["chunks"] = value
-
-    @property
-    def messages(self):
-        try:
-            return self.env["messages"]
-        except KeyError:
-            messages = []
-            self.env["messages"] = messages
-            return messages
-
-    @messages.setter
-    def messages(self, value):
-        self.env["messages"] = value
-
-    @property
-    def in_transfer_state(self):
-        try:
-            return self.env["in_transfer_state"]
-        except KeyError:
-            self.env["in_transfer_state"] = False
-            return False
-
-    @in_transfer_state.setter
-    def in_transfer_state(self, value):
-        self.env["in_transfer_state"] = bool(value)
 
     def data_received(self, data):
         """Called when some data is received.
         """
+        # restart the timer
         self.timer.cancel()
+        self.timer = self.loop.call_later(self.timeout, self.on_timeout)
+
         logger.debug("-> Data received from {!s}: {!r}".format(
             self.client, data))
         response = self.handle_data(data)
@@ -135,11 +91,13 @@ class ASTMProtocol(asyncio.Protocol):
         return response
 
     def default_handler(self, data):
+        """Default callback
+        """
         # raise ValueError("Unable to dispatch data: %r", data)
         logger.error("Unable to dispatch data: %r", data)
 
     def on_enq(self, data):
-        """Calls on <ENQ> message receiving.
+        """Callback when <ENQ> was received
         """
         logger.debug("on_enq: %r", data)
         if not self.in_transfer_state:
@@ -162,28 +120,28 @@ class ASTMProtocol(asyncio.Protocol):
     def on_eot(self, data):
         """Calls on <EOT> message receiving."""
         logger.debug("on_eot: %r", data)
-        if self.in_transfer_state:
-            # LIS-2A compliant message
-            lis2a_message = b""
-            # Raw ASTM message (including STX, sequence and checksum)
-            astm_message = b""
-
-            for record in self.messages:
-                seq, msg, cs = self.split_message(record)
-                lis2a_message += msg
-                astm_message += record
-
-            # Process either LIS-2A compliant or the raw message
-            if self.message_format == "astm":
-                self.queue.put_nowait(astm_message)
-            else:
-                self.queue.put_nowait(lis2a_message)
-
-            # Store the raw message for debugging and development purposes
-            self.log_message(astm_message)
-            self.discard_env()
-        else:
+        if not self.in_transfer_state:
             raise InvalidState("Server is not ready to accept EOT message.")
+
+        # LIS-2A compliant message
+        lis2a_message = b""
+        # Raw ASTM message (including STX, sequence and checksum)
+        astm_message = b""
+
+        for record in self.messages:
+            seq, msg, cs = self.split_message(record)
+            lis2a_message += msg
+            astm_message += record
+
+        # Process either LIS-2A compliant or the raw message
+        if self.message_format == "astm":
+            self.queue.put_nowait(astm_message)
+        else:
+            self.queue.put_nowait(lis2a_message)
+
+        # Store the raw message for debugging and development purposes
+        self.log_message(astm_message)
+        self.discard_env()
 
     def log_message(self, message, directory="astm_messages"):
         """Store the raw ASTM message if the folder exists in the CWD
@@ -194,14 +152,16 @@ class ASTMProtocol(asyncio.Protocol):
             write_message(message, path)
 
     def on_timeout(self):
-        """Calls when timeout event occurs. Used to limit waiting time for
-        response data."""
-        logger.debug("on_timeout")
+        """Callback for timeout event
+        """
+        logger.warning("Connection for {!r} timed out after {!r}s: Closing..."
+                       .format(self.client, self.timeout))
         self.discard_env()
         self.transport.close()
 
     def on_message(self, data):
-        """Calls on ASTM message receiving."""
+        """Callback when a message was received
+        """
         logger.debug("on_message: %r", data)
         if not self.in_transfer_state:
             self.discard_chunked_messages()
@@ -283,11 +243,13 @@ class ASTMProtocol(asyncio.Protocol):
     def discard_env(self):
         """Flush environment
         """
-        self.environ.pop(self.client, None)
+        self.chunks = []
+        self.messages = []
+        self.in_transfer_state = False
 
     def connection_lost(self, ex):
         """Called when the connection is lost or closed.
         """
-        logger.debug("Connection lost: {!s}".format(self.client))
+        logger.warning("Lost connection for {!s}".format(self.client))
         self.discard_env()
         self.transport.close()
