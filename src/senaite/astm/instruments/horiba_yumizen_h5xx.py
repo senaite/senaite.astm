a0408bd0d70422ee96c61ff8b02328883037f6df
Ramon Bartl <rb@ridingbytes.com>
2023-09-20 15:25:44 +0200

    Added Yumizen result schema

--- src/senaite/astm/instruments/horiba_yumizen_h5xx.py
+++ src/senaite/astm/instruments/horiba_yumizen_h5xx.py
@@ -2,6 +2,7 @@
 
 from senaite.astm import records
 from senaite.astm.fields import ComponentField
+from senaite.astm.fields import DateTimeField
 from senaite.astm.fields import NotUsedField
 from senaite.astm.fields import SetField
 from senaite.astm.fields import TextField
@@ -64,6 +65,39 @@ class CommentRecord(records.CommentRecord):
 class ResultRecord(records.ResultRecord):
     """Record to transmit analytical data.
     """
+
+    test = ComponentField(
+        Component.build(
+            NotUsedField(name='_'),
+            NotUsedField(name='__'),
+            NotUsedField(name='___'),
+            TextField(name='result_name'),
+            TextField(name='assay_code'),
+            TextField(name='dilution'),
+        ))
+    value = TextField()
+    units = TextField()
+    references = ComponentField(
+        Component.build(
+            TextField(name='result_range'),
+            TextField(name='range_name'),
+        ))
+    abnormal_flag = SetField(
+        field=TextField(),
+        length=4,
+        values=("HH", "H", "N", "L", "LL"))
+    status = SetField(
+        field=TextField(),
+        length=1,
+        values=("W", "X", "F"))
+    operator = ComponentField(
+        Component.build(
+            TextField(name='login'),
+            NotUsedField(name='_'),
+            TextField(name='profile'),
+        ))
+    started_at = DateTimeField()
+    completed_at = DateTimeField()
 
 
 class RequestInformationRecord(records.RequestInformationRecord):
