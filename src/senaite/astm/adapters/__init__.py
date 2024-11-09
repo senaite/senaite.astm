ff17213421e0e0bfe5cf748b990f426eaadfe51d
Ramon Bartl <rb@ridingbytes.com>
2024-11-09 22:48:24 +0100

    Ingore unused import

--- src/senaite/astm/adapters/__init__.py
+++ src/senaite/astm/adapters/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from senaite.astm.adapters import spotchem  # noqa
+from senaite.astm.adapters import spotchem    # noqa: F401
--- src/senaite/astm/adapters/spotchem/__init__.py
+++ src/senaite/astm/adapters/spotchem/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import se1520
+from . import se1520  # noqa: F401
